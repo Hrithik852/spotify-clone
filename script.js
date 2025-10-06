c34c3f047af8a5747cf6972107de92b2882766e7
HRITHIK suresh <hrithiksuresh852@gmail.com>
2025-10-06 19:56:54 +0530

    .

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("hello")
+async function main()
+{
+  let x =await fetch("http://127.0.0.1:5500/songs/");
+  let y=await x.text()
+  console.log(y);
+  let z=new Audio("/songs/all%20the%20stars.flac")
+  z.play();
+  z.pause()
+}
+main()
